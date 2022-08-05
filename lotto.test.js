a54297551a6458cdb728f34ba33c54308f98a93e
Charlie <ickyicky@gmail.com>
2022-08-05 11:59:40 +0900

    로또의 최고 순위와 최저 순위 풀기

---
+++ lotto.test.js
@@ -0,0 +1,84 @@
+function getLottoRank(lottos, winNumbers) {
+  return [getTopRank(lottos, winNumbers), getLowestRank(lottos, winNumbers)];
+}
+
+function getTopRank(lottos, winNumbers) {
+  return getNumberOfMatchingNumbers(
+    getMatchNumbers(lottos, winNumbers) + getNumberOfZero(lottos)
+  );
+}
+
+function getLowestRank(lottos, winNumbers) {
+  return getNumberOfMatchingNumbers(getMatchNumbers(lottos, winNumbers));
+}
+
+function getMatchNumbers(lottos, winNumbers) {
+  return lottos.filter((lottoNumber) => winNumbers.includes(lottoNumber))
+    .length;
+}
+
+function getNumberOfMatchingNumbers(count) {
+  return (
+    {
+      2: 5,
+      3: 4,
+      4: 3,
+      5: 2,
+      6: 1,
+    }[count] || 6
+  );
+}
+
+function getNumberOfZero(array) {
+  return array.filter((item) => item === 0).length;
+}
+
+describe('getLottoRank', () => {
+  it('숫자가 2개인 배열을 리턴한다.', () => {
+    const lottos = [45, 4, 35, 20, 3, 9];
+    const winNumbers = [20, 9, 3, 45, 4, 35];
+
+    expect(getLottoRank(lottos, winNumbers)).toHaveLength(2);
+  });
+
+  it('로또 번호와 당첨번호를 입력하면 순위를 리턴한다', () => {
+    const lottos = [45, 4, 35, 20, 3, 9];
+    const winNumbers = [20, 9, 3, 45, 4, 35];
+
+    expect(getLottoRank(lottos, winNumbers)).toEqual([1, 1]);
+  });
+
+  it.only('0이 6개면 [1,6] 을 리턴한다.', () => {
+    const lottos = [0, 0, 0, 0, 0, 0];
+    const winNumbers = [20, 9, 3, 45, 4, 35];
+
+    expect(getLottoRank(lottos, winNumbers)).toEqual([1, 6]);
+  });
+});
+
+describe('getNumberOfZero', () => {
+  it('0의 개수를 리턴한다.', () => {
+    const lottos = [0, 0, 0, 0, 0, 0];
+
+    expect(getNumberOfZero(lottos)).toEqual(6);
+  });
+});
+
+describe('getMatchNumbers', () => {
+  it('0을 제외한 일치하는 번호의 개수를 리턴한다', () => {
+    expect(getMatchNumbers([1, 2], [1, 2])).toBe(2);
+    expect(getMatchNumbers([1, 2], [1, 3])).toBe(1);
+  });
+});
+
+describe('getNumberOfMatchingNumbers', () => {
+  it('당첨 개수를 입력하면 순위를 리턴한다', () => {
+    expect(getNumberOfMatchingNumbers(0)).toBe(6);
+    expect(getNumberOfMatchingNumbers(1)).toBe(6);
+    expect(getNumberOfMatchingNumbers(2)).toBe(5);
+    expect(getNumberOfMatchingNumbers(3)).toBe(4);
+    expect(getNumberOfMatchingNumbers(4)).toBe(3);
+    expect(getNumberOfMatchingNumbers(5)).toBe(2);
+    expect(getNumberOfMatchingNumbers(6)).toBe(1);
+  });
+});
